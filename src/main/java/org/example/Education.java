f84b69a06c3900c71c7c5dd3da5167d1cd95621c
Alex <alexbatul@mail.ru>
2024-02-19 22:20:41 +0600

    Change

--- src/main/java/org/example/Education.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public enum Education {
-    ELEMENTARY,
-    SECONDARY,
-    FURTHER,
-    HIGHER
-}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.function.BinaryOperator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.Calculator;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    public void positiveTestAll() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(4, calculator.plus.apply(2, 2));
+        Assertions.assertEquals(6, calculator.minus.apply(8, 2));
+        Assertions.assertEquals(36, calculator.multiply.apply(6, 6));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void negativeTestAll() {
+        Calculator calculator = new Calculator();
+        Assertions.assertNotEquals(5, calculator.plus.apply(2, 2));
+        Assertions.assertNotEquals(9, calculator.minus.apply(8, 2));
+        Assertions.assertNotEquals(28, calculator.multiply.apply(6, 6));
+    }
+
+}
