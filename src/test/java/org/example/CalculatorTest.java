c870d3a12c1659acea2d714978f03fc8926408d5
Alex <alexbatul@mail.ru>
2024-02-22 17:37:21 +0600

    change

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.*;
 
 import java.util.function.BinaryOperator;
 
@@ -13,19 +11,41 @@ import org.example.Calculator;
 class CalculatorTest {
 
     @org.junit.jupiter.api.Test
-    public void positiveTest() {
+    public void testPlusPositive() {
         Calculator calculator = new Calculator();
         Assertions.assertEquals(4, calculator.plus.apply(2, 2));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMinusPositive() {
+        Calculator calculator = new Calculator();
         Assertions.assertEquals(6, calculator.minus.apply(8, 2));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMultiplyPositive() {
+        Calculator calculator = new Calculator();
         Assertions.assertEquals(36, calculator.multiply.apply(6, 6));
     }
 
     @org.junit.jupiter.api.Test
-    public void negativeTest() {
+    public void testPlusNegative() {
         Calculator calculator = new Calculator();
         Assertions.assertNotEquals(5, calculator.plus.apply(2, 2));
-        Assertions.assertNotEquals(9, calculator.minus.apply(8, 2));
-        Assertions.assertNotEquals(28, calculator.multiply.apply(6, 6));
     }
 
+    @org.junit.jupiter.api.Test
+    public void testMinusNegative() {
+        Calculator calculator = new Calculator();
+        Assertions.assertNotEquals(9, calculator.minus.apply(8, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMultiplyNegative() {
+        Calculator calculator = new Calculator();
+        Assertions.assertNotEquals(28, calculator.multiply.apply(6, 6));
+    }
 }
+
